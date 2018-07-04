b63e8ba6bdaf1191d145a5160a2d2a63b02849ed
Jeff Scott Brown <brownj@objectcomputing.com>
2018-07-04 17:59:05 -0500

    remove trailing slash
    Related to https://github.com/micronaut-projects/micronaut-core/issues/349

--- music-service/src/main/java/music/service/search/ItunesClient.java
+++ music-service/src/main/java/music/service/search/ItunesClient.java
@@ -3,7 +3,7 @@ package music.service.search;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.Client;
 
-@Client("https://itunes.apple.com/")
+@Client("https://itunes.apple.com")
 public interface ItunesClient {
 
     @Get("/search?limit=25&media=music&entity=album&term={term}")
