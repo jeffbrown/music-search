0c8fd0e953d3bc43a1f8842782da3f8542e81d26
Zachary Klein <zak@silver-chalice.com>
2018-07-05 16:34:58 -0500

    Made maxResults arg nullable

--- music-service/src/main/java/music/service/MusicController.java
+++ music-service/src/main/java/music/service/MusicController.java
@@ -4,9 +4,11 @@ import io.micronaut.core.util.StringUtils;
 import io.micronaut.http.HttpParameters;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 import music.service.search.ItunesClient;
 import music.service.search.SearchResult;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 @Controller("/music")
@@ -19,8 +21,7 @@ public class MusicController {
     }
 
     @Get("/search/{searchTerm}")
-    public List<Album> search(String searchTerm, HttpParameters parameters) {
-        String maxResults = parameters.get("maxResults");
+    public List<Album> search(String searchTerm, @Nullable @QueryValue String maxResults) {
         final int limit;
         if(StringUtils.isDigits(maxResults)) {
             limit = Math.min(25, Integer.parseInt(maxResults));
