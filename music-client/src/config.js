aa0adae5c86194a52b9caa1fe2389b43597a04e7
Zachary Klein <zak@silver-chalice.com>
2018-07-05 12:24:56 -0500

    Remove extra quotes

--- music-client/src/config.js
+++ music-client/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DEV_SERVER_URL: '"http://localhost:8080"',
-  PROD_SERVER_URL: '""' //server root
+  DEV_SERVER_URL: 'http://localhost:8080',
+  PROD_SERVER_URL: '' //server root
 }
